52fceba251d81f80bb49144251c5cb5b0134df7a
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-04-11 00:49:25 -0500

    bug arreglado

--- sPYam_py3.py
+++ sPYam_py3.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
 	print("")
 	getpass.getpass("presiona enter para continuar...")
 	subprocess.call(["cmd.exe","/c","cls"])
-	user = str(raw_input("introduzca su correo electronico: "))
+	user = str(input("introduzca su correo electronico: "))
 	passw = getpass.getpass("introduzca su clave: ")
-	spamlist = str(raw_input("intrduzca la ruta del archivo .txt con la lista de correos: "))
-	fmsg = str(raw_input("intrduzca la ruta de un archivo .txt con el mensaje: "))
+	spamlist = str(input("intrduzca la ruta del archivo .txt con la lista de correos: "))
+	fmsg = str(input("intrduzca la ruta de un archivo .txt con el mensaje: "))
 	mails = []
 	spamfile = open(spamlist, 'r')
 	for line in spamlist:
@@ -41,9 +41,10 @@ if __name__ == '__main__':
 	for line in fmsg:
 		msg += line
 	rmsg.close()
-	ghmail = str(raw_input("si quieres usar hotmail pon 'h' y 'g' para gmail: "))	
+	ghmail = str(input("si quieres usar hotmail pon 'h' y 'g' para gmail: "))	
 
 	if ghmail == 'g':
 		send_mail(user, passw, msg, spamlist, 'smtp.gmail.com:587')
 	elif ghmail == 'h':
 		send_mail(user, passw, msg, spamlist, 'smtp.live.com:25')
+	
