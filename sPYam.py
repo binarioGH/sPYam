b0178eafd3b19a765ef5aacf6ecef1d7b0101a63
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-06-12 15:39:25 -0500

    Ahora se puede mandar correos por outlook o hotmail. se arreglaron algunos problemas.

--- sPYam.py
+++ sPYam.py
@@ -16,7 +16,7 @@ def text(msgfile):
 	file.close()
 	return msgstr
 
-def unabombermod(mailist, msg, usr, psw, rge):
+def unabombermod(mailist, msg, usr, rge):
 	for mail in mailist:
 		for num in range(rnge):
 			try:
@@ -35,6 +35,7 @@ def h():
 	print("-mfile:\nEstablecer archivo de correos.\n")
 	print("\n\nEsta bandera es solo si está activado el modo unabomber:")
 	print("-r:\nEstablecer la cantidad de veces que se enviará (si no lo usas se mandará una vez). ")
+	print("-gm:\nSi tu correo es de gmail debes de usar esta bandera.\n-hm:\nSi tu correo es de hotmail.")
 if __name__ == '__main__':
 	if len(argv) <= 1:
 		print("{} necesita más argumentos para funcionar, usa:\n{} -h para ver las opciones.".format(argv[0],argv[0]))
@@ -44,12 +45,17 @@ if __name__ == '__main__':
 		unabomber = False
 		user, passw, msgfile =  str(), str(), str()
 		rnge = 1
+		gm, hm = False, False
 		for arg in argv:
 			if arg[0] != "-":
 				count += 1
 				continue
 			if arg == "--unabomber":
 				unabomber = True
+			elif arg == "-gm":
+				gm = True
+			elif arg == "-hm":
+				hm = True
 			elif arg == "-h":
 				h()
 				exit()
@@ -75,8 +81,18 @@ if __name__ == '__main__':
 				print("No se reconoce la bandera '{}'".format(arg))
 				exit()
 			count += 1
+		if gm == True and hm == True:
+			print("No se puede usar -gm y -hm al mismo tiempo.")
+			exit()
+		elif gm == True:
+			smtpserver = 'smtp.gmail.com:587'
+		elif hm == True:
+			smtpserver = 'smtp.live.com:587'
+		else:
+			print("No se ha establecido si se usara gmail o hotmail.")
+			exit()
 		try:
-			server = SMTP('smtp.gmail.com:587')
+			server = SMTP(smtpserver)
 			server.starttls()
 		except Exception as e:
 			print("problemas al iniciar el servidor:\n{}".format(e))
@@ -87,6 +103,7 @@ if __name__ == '__main__':
 			print("No se ha podido iniciar seción:\n{}".format(e))
 			exit()
 		msgstr = text(msgfile)
-		unabombermod(mails, msgstr, user, passw, rnge)
+		
+		unabombermod(mails, msgstr, user, rnge)
 		
 			
